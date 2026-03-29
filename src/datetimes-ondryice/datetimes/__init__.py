3e664b5c72cee6ce63d43344475e7650adb358da
ondryice <andreas.johnson@anicius.com>
2026-03-28 18:20:28 -0600

    builtins for module

--- src/datetimes-ondryice/datetimes/__init__.py
+++ src/datetimes-ondryice/datetimes/__init__.py
@@ -1,6 +1,16 @@
+from datetime import date, datetime, time, timedelta, timezone, UTC
+
 from datetimes.dateranges import daterange
 
 __all__ = [
   # classes
   'daterange',
+
+  # builtins
+  'date',
+  'datetime',
+  'time',
+  'timedelta',
+  'timezone',
+  'UTC',
 ]
