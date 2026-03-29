7cd373eb7e04bcd5a88c89cf04c7e347fb5230ad
ondryice <andreas.johnson@anicius.com>
2026-03-28 18:21:23 -0600

    tz-aware currtime funcs

--- src/datetimes-ondryice/datetimes/misc.py
+++ src/datetimes-ondryice/datetimes/misc.py
@@ -3,3 +3,9 @@ from datetime import datetime, UTC
 PROGRAM_START = datetime.now(UTC).astimezone()
 LOCAL_TIME = PROGRAM_START.tzinfo
 
+def now (tz=...):
+  if tz is ...:
+    tz = LOCAL_TIME
+  return datetime.now(tz)
+def today (tz=...):
+  return now(tz).date()
