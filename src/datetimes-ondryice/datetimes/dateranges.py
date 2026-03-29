35a7801208e75071549fb89081bb2dd7b190fd58
ondryice <andreas.johnson@anicius.com>
2026-03-28 18:07:37 -0600

    daterange init

--- src/datetimes-ondryice/datetimes/dateranges.py
+++ src/datetimes-ondryice/datetimes/dateranges.py
@@ -11,3 +11,17 @@ class daterange (Sequence[date]):
     )
     if len(args) not in (2,3):
       raise TypeError(f"invalid number of arguments {len(args)} - expected 2 or 3; {USAGE}")
+    a1, a2, step = [ *args, 1 ][:3]
+    start = stop = None
+    if isinstance(a1, date):
+      start = a1.toordinal()
+      if isinstance(a2, date):
+        stop = a2.toordinal()
+      elif isinstance(a2, int):
+        stop = start + a2 * step
+    elif isinstance(a1, int) and isinstance(a2, date):
+      stop = a2.toordinal()
+      start = stop - a1 * step
+    if start is None or stop is None:
+      raise TypeError(f"invalid types provided {args} - {USAGE}")
+    self._r = range(start, stop, step)
