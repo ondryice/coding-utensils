06304e33d980bcaba67966d83c1ff0c00e37e615
ondryice <andreas.johnson@anicius.com>
2026-03-29 18:01:48 -0600

    timedelta rounding func

--- src/datetimes-ondryice/datetimes/misc.py
+++ src/datetimes-ondryice/datetimes/misc.py
@@ -1,4 +1,4 @@
-from datetime import datetime, UTC
+from datetime import datetime, timedelta, UTC
 
 PROGRAM_START = datetime.now(UTC).astimezone()
 LOCAL_TIME = PROGRAM_START.tzinfo
@@ -9,3 +9,30 @@ def now (tz=...):
   return datetime.now(tz)
 def today (tz=...):
   return now(tz).date()
+
+def td_rounded (value, spec: str, /):
+  from datetimes.timedelta_tuples import TimeDeltaTuple as tdt
+  if isinstance(value, timedelta):
+    delta = tdt.new(value)
+  elif isinstance(value, tdt):
+    delta = value
+  else:
+    raise TypeError(f"invalid type for value {value!r} - {type(value).__name__} is not supported")
+  spec = spec.lower()
+  spec = dict(
+    microsecond='us', millisecond='ms', second='ss',
+    minute='mm', hour='hh', day='dd',
+  ).get(spec.removesuffix('s'), dict(
+    micro='us', milli='ms', sec='ss', min='mm', hr='hh',
+  ).get(spec, spec))
+  def result (dd=0, hh=0, mm=0, ss=0, ms=0, us=0):
+    if isinstance(value, timedelta): return timedelta(dd, ss, us, ms, mm, hh)
+    return tdt(dd, hh, mm, ss, ms, us)
+  match (spec):
+    case 'us': return result(us=delta.asmicroseconds())
+    case 'ms': return result(ms=round(delta.asmilliseconds()))
+    case 'ss': return result(ss=round(delta.asseconds()))
+    case 'mm': return result(mm=round(delta.asminutes()))
+    case 'hh': return result(hh=round(delta.ashours()))
+    case 'dd': return result(dd=round(delta.asdays()))
+  raise ValueError(f"invalid spec {spec!r} - must be 'dd', 'hh', 'mm', 'ss', 'ms', or 'us', or an alias of one of these")
--- src/datetimes-ondryice/datetimes/misc.pyi
+++ src/datetimes-ondryice/datetimes/misc.pyi
@@ -1,6 +1,9 @@
 """Basic module for constants & functions involving date/time objects and operations."""
 
-from datetime import date, datetime, timezone, UTC
+from datetime import date, datetime, timedelta, timezone, UTC
+from typing import Literal, overload
+
+from datetimes.timedelta_tuples import TimeDeltaTuple
 
 UTC
 PROGRAM_START: datetime
@@ -12,3 +15,25 @@ def now (tzinfo = LOCAL_TIME, /) -> datetime:
   """Returns the current date/time."""
 def today (tzinfo = LOCAL_TIME, /) -> date:
   """Returns the current date, taking timezone into account."""
+
+@overload
+def td_rounded (timedelta: timedelta, spec: Literal['dd','hh','mm','ss','ms','us'], /) -> timedelta:
+  """Rounds the provided timedelta to the nearest unit of measure of the provided specificity."""
+@overload
+def td_rounded (tuple: TimeDeltaTuple, spec: Literal['dd','hh','mm','ss','ms','us'], /) -> TimeDeltaTuple: ...
+@overload
+def td_rounded (
+  timedelta: timedelta,
+  spec: Literal['days','hours','minutes','seconds','milliseconds','microseconds',
+    'day','hour','minute','second','millisecond','microsecond',
+    'day','hr','min','sec','milli','micro'
+  ],
+/) -> timedelta: ...
+@overload
+def td_rounded (
+  tuple: TimeDeltaTuple,
+  spec: Literal['days','hours','minutes','seconds','milliseconds','microseconds',
+    'day','hour','minute','second','millisecond','microsecond',
+    'day','hr','min','sec','milli','micro'
+  ],
+/) -> TimeDeltaTuple: ...
