fa7efcac83c41299e72bc99501b0b19670f5d4ff
ondryice <andreas.johnson@anicius.com>
2026-04-02 14:15:04 -0600

    muting & file blockers

--- src/logs-ondryice/logs/_logs.py
+++ src/logs-ondryice/logs/_logs.py
@@ -14,6 +14,23 @@ class log:
     raise TypeError(f"cannot get super log for {cls.__name__} - not a subclass of log")
 
   @classmethod
+  def mute (cls):
+    cls.muted += 1
+    return cls
+  @classmethod
+  def unmute (cls):
+    cls.muted -= cls.muted > 0
+    return cls
+  @classmethod
+  def block (cls):
+    cls.blocked += 1
+    return cls
+  @classmethod
+  def unblock (cls):
+    cls.blocked -= cls.blocked > 0
+    return cls
+
+  @classmethod
   def push (cls, *values, sep=' ', end='\n', flush=False):
     if not cls.super():
       if not cls.blocked:
