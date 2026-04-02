3cb33a6fbf858644a8eb52a0e9b37653b44a5b4c
ondryice <andreas.johnson@anicius.com>
2026-04-02 12:54:41 -0600

    changed to locking mechanism instead of per-file blocking

--- src/logs-ondryice/logs/_files.py
+++ src/logs-ondryice/logs/_files.py
@@ -8,7 +8,7 @@ class LOGFILE:
   file: None|TextIOWrapper
   times: list[datetime]
   indent: int
-  blocked: int
+  locked: int
   def __new__ (cls, file, start=..., /):
     if file is None: return STDOUT
     elif isinstance(file, TextIOWrapper) and file.writable():
@@ -19,20 +19,24 @@ class LOGFILE:
     obj = super().__new__(LOGFILE)
     obj.file = file
     obj.times = [ start ]
-    obj.indent = obj.blocked = 0
+    obj.indent = obj.locked = 0
     LOGFILE.__logfile_instances[file] = obj
     return obj
-  def block (self):
-    self.blocked += 1
+  def lock (self):
+    self.locked += 1
     return self
-  def unblock (self):
-    self.blocked -= self.blocked > 0
+  def unlock (self):
+    self.locked -= self.locked > 0
     return self
   def enter (self, instant=...):
+    if self.locked:
+      return self
     self.times.append(_inst(instant))
     self.indent += 1
     return self
   def escape (self):
+    if self.locked:
+      return self
     if not self.indent:
       raise RuntimeError("cannot escape section - output already at zero-level indent")
     self.times.pop()
@@ -40,10 +44,12 @@ class LOGFILE:
     return self
 
   def write (self, *values, sep=' ', end='\n', flush=False):
+    if self.locked:
+      return self
     print(*values, sep=sep, end=end, file=self.file, flush=flush)
     return self
 
 STDOUT = object.__new__(LOGFILE)
 STDOUT.file, STDOUT.times = None, [ PROGRAM_START ]
-STDOUT.indent = STDOUT.blocked = 0
+STDOUT.indent = STDOUT.locked = 0
 LOGFILE.__logfile_instances = {}
