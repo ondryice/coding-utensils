6e6a5de882e68e5ff54ff48dfd8a72a60f486169
ondryice <andreas.johnson@anicius.com>
2026-04-02 10:43:02 -0600

    logfile ctor

--- src/logs-ondryice/logs/_files.py
+++ src/logs-ondryice/logs/_files.py
@@ -1,8 +1,20 @@
 from datetime import datetime
 from io import TextIOWrapper
 
+from logs._misc import instant as _inst, PROGRAM_START
+
 class LOGFILE:
   file: None|TextIOWrapper
   times: list[datetime]
   indent: int
   blocked: int
+  def __new__ (cls, file, start=..., /):
+    if file is None: pass
+    elif isinstance(file, TextIOWrapper) and file.writable(): pass
+    else: raise ValueError(f"invalid file {file!r} - must be None for stdout or writable instance of io.TextIOWrapper")
+    start = PROGRAM_START if start is ... else _inst(start)
+    obj = super().__new__(LOGFILE)
+    obj.file = file
+    obj.times = [ start ]
+    obj.indent = obj.blocked = 0
+    return obj
