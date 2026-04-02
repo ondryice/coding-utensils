3426516be560793c086e9062b9ab37cd58981a1e
ondryice <andreas.johnson@anicius.com>
2026-04-02 12:48:45 -0600

    basis for forwarding statements from derived (sub) logs

--- src/logs-ondryice/logs/_logs.py
+++ src/logs-ondryice/logs/_logs.py
@@ -2,9 +2,20 @@ from logs._files import STDOUT
 
 class LOGBASE:
   output = STDOUT
+  forwarding = False
+
+  @classmethod
+  def super (cls) -> None|type[LOGBASE]:
+    if cls is LOGBASE:
+      return None
+    elif issubclass(cls, LOGBASE):
+      return cls.__base__
+    raise TypeError(f"cannot get super log for {cls.__name__} - not a subclass of log")
 
   @classmethod
   def push (cls, *values, sep=' ', end='\n', flush=False):
     if not cls.output.blocked:
       cls.output.write(*values, sep=sep, end=end, flush=flush).block()
-    return cls
+    if cls.forwarding and cls.super():
+      cls.super().push(*values, sep=' ', end='\n', flush=flush)
+    
